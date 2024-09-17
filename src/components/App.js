a3bc44cb48f8f36ee087c96009246232b92b23b6
manishamali1525 <manishamali1525@users.noreply.github.com>
2024-09-17 11:20:02 +0530

    updated files

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,18 @@
  import React from "react";
  import {useState} from "react";
-import './../styles/App.css';
 
 const App = () => {
-  const [name,setName] = useState("");
+  const [name,setName] = useState();
   function handleInputChange(e){
     setName(e.target.value)
   }
   return (
-    <div id="main">
+    <div>
         {/* Do not remove the main div */}
         <label htmlFor="ip">Enter your name:</label> <br/>
-        <input 
-          id="ip"
-          type="text"
-          value={name}
-          onChange={handleInputChange}
+        <input id="ip" type="text" value={name} onChange={handleInputChange}
         />
-        <p>{name && `Hello {name}!`}</p>
+        <p>{name && `Hello ${name}!`}</p>
     </div>
   )
 }
